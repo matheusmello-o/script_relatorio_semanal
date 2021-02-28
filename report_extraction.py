a1e065bb945ba537c3dae895f141727fb03a75b5
Matheus Mello <matheusmellocunha@gmail.com>
2021-02-28 04:02:51 -0300

    Created the column that informed the type of video and divided the column Video Playtime by 3600 to get hours.

--- report_extraction.py
+++ report_extraction.py
@@ -3,17 +3,36 @@ from glob import glob
 
 # Script instructions
 print('\n\nAqui será feito a extracao do relatório semanal.')
-print('Por favor, digite a data conforme a o modelo a seguir de ano, mês e dia: 20210108\n')
+print('Por favor, digite a data conforme a o modelo a seguir de ano, mês e dia:--> 20210108')
+print('E a semana conforme o exemplo a seguir:--> Semana 2\n')
 
 date_min = input('Digite a data que inicia a semana: ')
 date_max = input('Digite a data que encerra a semana: ')
+week_number = input('Qual seria a semana: ')
 
 # Catching all files in the path. After that, all sort all the files in a list
-files = sorted(glob(r'base/*.csv'))
+files_assinantes = sorted(glob(r'base_assinantes/*.csv'))
+files_logados_free = sorted(glob(r'base_lf/*.csv'))
 
 # Reading all the files and concating them in a dataframe
-df = pd.concat(pd.read_csv(file) for file in files)
+df_a = pd.concat(pd.read_csv(file) for file in files_assinantes)
+df_lf = pd.concat(pd.read_csv(file) for file in files_logados_free)
 
+df_a['tipo_de_consumo'] =\
+['VOD Aberto' if tipo_video[1:] == 'berto' else\
+'live' if tipo_video == 'live' else\
+'VOD Fechado' for tipo_video in df['Video - Tipo de Vide']]
+
+df_lf['tipo_de_consumo'] = 'VOD Logado Free'
+
+# Creating the main dataframe
+df = pd.concat([df_a, df_lf])
+df['semana'] = week_number
+
+df['horas_consumidas'] = df['Video Playtime'] / 3600
+
+
+df_group = df.groupby(['Video - ID do programa', ])
 
 
 
